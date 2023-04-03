80ae73983e21375afb73b9919b7983d74827140c
Alexey Strutsky <alexeystrutsky5550@mail.ru>
2023-04-03 21:11:17 +0300

    реализован вертикалиный аккордеон jQuerry, работа слайдера bxSlider

--- js/tabs.js
+++ js/tabs.js
@@ -4,6 +4,12 @@ const findBlockByAlias = (alias) => {
   });
 };
 
+const findByAutor = (mean) => {
+  return $('.autor-mobile').filter((ndx, item) => {
+    return $(item).attr('data-author') == mean;
+  });
+};
+
 $('.reviews_switcher__link').on('click', e => {
   e.preventDefault();
 
@@ -11,7 +17,11 @@ $('.reviews_switcher__link').on('click', e => {
   const target = $this.attr('data-open');
   const itemToShow = findBlockByAlias(target);
   const curItem = $this.closest('.reviews_switcher__item');
-  
+
+  const mobileAutor = $this.attr('data-author');
+  const autorToShow = findByAutor(target);
+
   itemToShow.addClass('reviews__item--active').siblings().removeClass('reviews__item--active');
-  curItem.addClass('reviews_switcher__item--active').siblings().removeClass('reviews_switcher__item--active');
+  curItem.addClass('reviews_switcher__item--active').siblings().removeClass('reviews_switcher__item--active');  
+  autorToShow.addClass('reviews__author--active').siblings().removeClass('reviews__author--active');
 });
